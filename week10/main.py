0b6bd6c80576eb77561691d83bbe10fee19473ac
浩宇 <33846887+Haoyu2@users.noreply.github.com>
2024-04-06 23:01:00 -0500

    week10

--- week10/main.py
+++ week10/main.py
@@ -11,15 +11,14 @@ print('from cli update!')
 
 print('S3 upload event trigger added for function demo!')
 
+print('Deployed from Windows')
 
 def main(event, context):
     # TODO implement
     print('from local main.main zip package deployment ')
     print(event)
     print(context)
-    response = s3.list_buckets()
-    print(response)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Yes it is from Lambda')
+        'body': json.dumps('Hello from Lambda! Yes it is from Lambda! Deployed from Windows!')
     }
